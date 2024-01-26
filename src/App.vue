11f31221119f73948b8369ae7894877aa41a386b
James Paul Tulod <paultulod@pm.me>
2024-01-27 02:37:14 +0800

    Add TransactionList data and move to global state

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <Balance />
     <IncomeExpense />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
@@ -14,4 +14,13 @@ import Balance from './components/Balance.vue'
 import IncomeExpense from './components/IncomeExpense.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
+
+import { ref } from 'vue'
+
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+])
 </script>
